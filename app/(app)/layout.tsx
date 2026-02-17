aeec3290e7cde9f20bb4df0ea41c5b9432a5a2b5
Oscar Ceballos <oscaralbertoceballos@gmail.com>
2026-02-16 21:16:29 -0700

    testgh

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -4,7 +4,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SidebarNav />
-      <main className="pt-14 md:pt-0 md:pl-56">
+      <main className="pt-14 md:pt-0 md:pl-56 ">
         <div className="p-4 md:p-6">{children}</div>
       </main>
     </>
